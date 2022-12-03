5e6eaa697d3888772e53a8c03fe76c5b6d0c88ff
Mohamad Fadavi <fadavimohamad@gmail.com>
2022-12-03 13:18:29 +0300

    test: for github/search-users

---
+++ src/lib/github/search-users.spec.ts
@@ -0,0 +1,26 @@
+import * as sinon from 'sinon'
+import * as githubSearch from './search-users'
+import * as githubClient from './client'
+import { expect } from 'chai'
+import { GraphQLClient } from 'graphql-request'
+
+describe('lib/github/search-users', () => {
+  context('searchUsers', () => {
+    let getClientStub: sinon.SinonStub<[], GraphQLClient>
+
+    beforeEach(() => getClientStub = sinon.stub(githubClient, 'getClient'))
+    afterEach(() => sinon.restore())
+
+    it('makes a graphql request', async () => {
+      const fakeResponse = {}
+      const requestStub = sinon.stub().resolves(fakeResponse)
+
+      // @ts-expect-error-next-line
+      getClientStub.returns({ request: requestStub } as GraphQLClient)
+      const resp = await githubSearch.searchUsers({ query: 'language:c' })
+
+      expect(requestStub.calledOnce).to.be.true
+      expect(resp).to.be.equal(fakeResponse)
+    })
+  })
+})
